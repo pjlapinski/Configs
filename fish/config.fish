a25228e9fa1322d7695b595b276256f6b11bb3ef
pjlapinski <46348745+pjlapinski@users.noreply.github.com>
2021-09-27 23:28:20 +0200

    fish shell startup file

---
+++ fish/config.fish
@@ -0,0 +1,27 @@
+alias ls='exa --color=always --group-directories-first'
+alias tree='exa -T'
+abbr -a l ls
+abbr -a ll ls -alh --git
+abbr -a la ls -a
+abbr -a l. 'exa -Fa | egrep "^\."'
+
+set -gx PATH ~/.scripts/ $PATH
+
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+
+alias cp='cp -i'
+alias mv='mv -i'
+
+set -l recent_exec ~/.config/NotionTODO/.exec_time
+if not [ -e $recent_exec ]
+    touch $recent_exec
+    notion_todo
+end
+# Only run on startup once every 8 hours
+if [ (math (date +%s) - (stat --printf "%Y" $recent_exec)) -gt 28800 ]
+    touch $recent_exec
+    notion_todo
+end
+
