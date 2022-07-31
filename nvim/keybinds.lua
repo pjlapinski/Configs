d7aa6f1dba1bc77d35f4e321c1d74edd449f589a
Przemysław Łapiński <przemyslaw.j.lapinski@gmail.com>
2022-08-01 01:22:45 +0200

    Moved the neovim config to lua

---
+++ nvim/keybinds.lua
@@ -0,0 +1,65 @@
+-- Come on Vim, be consistent
+vim.api.nvim_set_keymap('n', 'Y', 'y$', { noremap = true })
+
+-- Keep cursor at the same position with J
+vim.api.nvim_set_keymap('n', 'J', 'mzJ`z', { noremap = true })
+
+-- Keep the search centered
+vim.api.nvim_set_keymap('n', 'n', 'nzzzv', { noremap = true })
+vim.api.nvim_set_keymap('n', 'N', 'Nzzzv', { noremap = true })
+
+-- Additional undo break points
+vim.api.nvim_set_keymap('i', ',', ',<C-g>u', { noremap = true })
+vim.api.nvim_set_keymap('i', '.', '.<C-g>u', { noremap = true })
+vim.api.nvim_set_keymap('i', '!', '!<C-g>u', { noremap = true })
+vim.api.nvim_set_keymap('i', '?', '?<C-g>u', { noremap = true })
+
+-- Moving text around
+vim.api.nvim_set_keymap('v', '<C-j>', ":m '>+1<CR>gv=gv", { noremap = true })
+vim.api.nvim_set_keymap('v', '<C-k>', ":m '<-2<CR>gv=gv", { noremap = true })
+vim.api.nvim_set_keymap('i', '<C-j>', "<esc>:m .+1<CR>==", { noremap = true })
+vim.api.nvim_set_keymap('i', '<C-k>', "<esc>:m .-2<CR>==", { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-j>', ":m .+1<CR>==", { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-k>', ":m .-2<CR>==", { noremap = true })
+
+vim.g.mapleader = ' '
+
+-- Kill the current buffer
+vim.api.nvim_set_keymap('n', '<leader>q', ":bd!<CR>", {})
+
+-- Navigation between buffers
+vim.api.nvim_set_keymap('n', '<leader>wn', "<C-w>n:Explore .<CR>", {})
+vim.api.nvim_set_keymap('n', '<leader>wv', "<C-w>v:Explore .<CR>", {})
+vim.api.nvim_set_keymap('n', '<leader>h', "<C-w>h", {})
+vim.api.nvim_set_keymap('n', '<leader>j', "<C-w>j", {})
+vim.api.nvim_set_keymap('n', '<leader>k', "<C-w>k", {})
+vim.api.nvim_set_keymap('n', '<leader>l', "<C-w>l", {})
+
+-- Cycle tabs
+vim.api.nvim_set_keymap('n', '<S-Tab>', 'gt', {})
+
+--- Plugin binds
+
+-- Open up LazyGit
+vim.api.nvim_set_keymap('n', '<leader>gg', ':LazyGit<CR>', { noremap = true, silent = true })
+
+-- Comment selected lines
+vim.api.nvim_set_keymap('n', '<C-_>', ":call nerdcommenter#Comment('n', 'Toggle')<CR>", {})
+vim.api.nvim_set_keymap('v', '<C-_>', ":call nerdcommenter#Comment('x', 'Toggle')<CR>", {})
+-- For neovide
+vim.api.nvim_set_keymap('n', '<C-/>', ":call nerdcommenter#Comment('n', 'Toggle')<CR>", {})
+vim.api.nvim_set_keymap('v', '<C-/>', ":call nerdcommenter#Comment('x', 'Toggle')<CR>", {})
+
+-- Open a new tab with file explorer
+vim.api.nvim_set_keymap('n', '<leader>tn', ":call nerdcommenter#Comment('x', 'Toggle')<CR>", {})
+
+-- Open NERDTree
+vim.api.nvim_set_keymap('n', '<C-n>', ':NERDTreeToggle<CR>', {})
+
+-- LSP
+vim.api.nvim_set_keymap('n', 'gd', ':lua vim.lsp.buf.definition()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'gD', ':lua vim.lsp.buf.declaration()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'gr', ':lua vim.lsp.buf.references()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'gi', ':lua vim.lsp.buf.implementation()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'K', ':lua vim.lsp.buf.hover()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-s>', ':lua vim.lsp.buf.signature_help()<CR>', { noremap = true, silent = true })
