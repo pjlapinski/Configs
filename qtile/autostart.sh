2153887c136d320b585ef9ecc74d86b57c02233a
Przemysław Łapiński <przemyslaw.j.lapinski@gmail.com>
2023-12-17 03:54:33 +0100

    Update autostart.sh

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -3,3 +3,6 @@
 nitrogen --restore &
 picom -b -f &
 caffeine &
+
+xrandr --output DP-0 --primary --mode 1920x1080 --rate 144.00 --right-of HDMI-0
+xrandr --output HDMI-0 --mode 1920x1080 --rate 60.00
